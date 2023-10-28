bf2009a08884df5f8d93b54eae881da391407e75
shuvo14051 <shuvo1137017@gmail.com>
2023-10-27 21:18:37 -0400

    About page done

--- src/App.js
+++ src/App.js
@@ -3,28 +3,50 @@ import "./App.css";
 import About from "./components/About";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
+import Alert from "./components/Alert";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState("light");
+  const [alert, setAlert] = useState("");
+
+  const showAlert = (message, type) => {
+    setAlert({
+      message: message,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert("");
+    }, 1500);
+  };
 
   const toogleMode = () => {
     if (mode === "light") {
       setMode("dark");
-      document.body.style.backgroundColor = "#333";
+      document.body.style.backgroundColor = "#13466e";
       document.body.style.color = "white";
+      showAlert("Dark mode has been enabled", "success");
     } else {
       setMode("light");
       document.body.style.backgroundColor = "white";
       document.body.style.color = "black";
+      showAlert("Light mode has been enabled", "success");
     }
   };
   return (
-    <div>
-      {/* <Navbar title="ShuvoBlog" /> */}
+    <BrowserRouter>
       <Navbar title="Textutils" mode={mode} toogleMode={toogleMode} />
-      <TextForm headline="Enter text to analyze" mode={mode} />
-      <About />
-    </div>
+      <Alert alert={alert} />
+      <Routes>
+        <Route exact path="/about" element={<About />} />
+        <Route
+          exact
+          path="/"
+          element={<TextForm headline="Enter your text to analyze" />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
